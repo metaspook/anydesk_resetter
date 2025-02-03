cfc4f3b9978fae8555dfa2e2fc58d5c92d9bbd46
metaspook <metaspook@gmail.com>
2025-02-04 00:17:30 +0600

    chore: remove process_target model

--- lib/shared/models/models.dart
+++ lib/shared/models/models.dart
@@ -1,2 +1,1 @@
-export 'process_target.dart';
 export 'typedefs.dart';
--- lib/shared/models/process_target.dart
+++
@@ -1,8 +0,0 @@
-class ProcessTarget {
-  const ProcessTarget({
-    required this.name,
-    required this.possibleNames,
-  });
-  final String name;
-  final List<String> possibleNames;
-}
